fe0a55d18af4a888102e430c4c773f7f08bff6cd
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-07-01 23:45:36 +0300

    realised collectionList view logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Petr', age: 55 });
-const userEdit = new UserEdit(document.getElementById('root'), user);
-userEdit.render();
+// const user = User.buildUser({ name: 'Petr', age: 55 });
+// const userEdit = new UserEdit(document.getElementById('root'), user);
+// userEdit.render();
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('list');
+  if (!root) return;
+  users.models.forEach(user => console.log(user));
+  const userList = new UserList(root, users);
+  userList.render();
+});
+
+users.fetch();
